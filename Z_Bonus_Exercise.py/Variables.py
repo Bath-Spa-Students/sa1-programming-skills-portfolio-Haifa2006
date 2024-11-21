00200b106f743cf86e43b9719401000ae5126464
Haifa2006 <fathimahaifahaneef.2006@gmail.com>
2024-11-21 21:08:00 +0400

    Variables.py

---
+++ Z_Bonus_Exercise.py/Variables.py
@@ -0,0 +1,16 @@
+#Variables:
+"""Assign a message to a variable, and print that message. Then change the value of the
+variable to a new message, and print the new message."""
+
+#Answers:
+#Storing a message in a variable
+coding_welcome = "Welcome to the world of coding!"
+
+#Printing coding_welcome
+print(coding_welcome)
+
+#Changing the value of the variable to a new message
+coding_welcome = "Where Python makes learning enjoyable!"
+
+#Printing new message
+print(coding_welcome)

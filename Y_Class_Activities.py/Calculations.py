81d4ca5f55c62c4e197742aba8a62f608da28a1f
Haifa2006 <fathimahaifahaneef.2006@gmail.com>
2024-11-21 20:38:57 +0400

    Calculations.py

---
+++ Y_Class_Activities.py/Calculations.py
@@ -0,0 +1,9 @@
+#Calculations
+
+#Performing Calculations through operators
+print (8+ 5) # For Addition
+print (12- 6) # For Substraction
+print (13 *3) # For Multiplication
+print (30/3)  # For floating point
+print (20//2) # For integer
+

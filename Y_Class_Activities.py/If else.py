c5854f2d993bea7a24b31db14c91606547681024
Haifa2006 <fathimahaifahaneef.2006@gmail.com>
2024-11-21 20:46:49 +0400

    If else.py

---
+++ Y_Class_Activities.py/If else.py
@@ -0,0 +1,13 @@
+#If else statement practice
+
+#Storing 250 in variable a
+a = 250
+#Storing 25 in variable b
+b = 25
+#Comparing if variable b is greater than variable a 
+if b > a:
+ #Print this message if the condition is true
+ print ("b is greater than a")
+ #Print this message if the condition is false
+else:
+ print("a is greater than b")

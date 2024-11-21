adcee19ceda9938dbbdacea6e6e62b83aaf696ea
Haifa2006 <fathimahaifahaneef.2006@gmail.com>
2024-11-21 21:16:25 +0400

    List - Names.py

---
+++ Z_Bonus_Exercise.py/List - Names.py
@@ -0,0 +1,14 @@
+#List - Names
+"""Store the names of a few of your friends in a list called names. Print each person’s name
+by accessing each element in the list, one at a time."""
+ 
+#Answers:
+#Storing the name of my friends in the list called names
+names = ["Shaazin", "Anaswara", "Sinan", "Huda", "Rifa"]
+  
+#Printing each person's name by accessing each element in the list, one at a time
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])

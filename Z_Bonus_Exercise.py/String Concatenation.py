e2a30675859d76149d6539c00d3fec4cd6e218e7
Haifa2006 <fathimahaifahaneef.2006@gmail.com>
2024-11-21 21:07:43 +0400

    String Concatenation.py

---
+++ Z_Bonus_Exercise.py/String Concatenation.py
@@ -0,0 +1,14 @@
+#String Concatenation:
+"""Write three strings in different variables and print the output as one string."""
+
+#Answers:
+#Creating three strings in different variables
+Word1 = "Welcome"
+Word2 = "To"
+Word3 = "Bathspa University"
+
+#Joining the strings together
+output = Word1 + " " + Word2 + " " + Word3
+
+#Printing the output
+print(output)

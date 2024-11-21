114bf5dc28130c684fd1c6e8c134b9aa2011ea09
Haifa2006 <fathimahaifahaneef.2006@gmail.com>
2024-11-21 20:46:07 +0400

    End.py

---
+++ Y_Class_Activities.py/End.py
@@ -0,0 +1,23 @@
+#End
+
+# end delimiter to remove new line , By default print will switch to another statement 
+name = "Fathima Haifa"
+print (name, end = " ")
+print  ("New print line message")
+
+# \n and \t These are allso known as escape sequences
+print ("\n this line will print on next line")
+print ("\t this is tabbed in")
+
+#Operator Precedence BODMAS (Bracket, order,division, multiplication, addition, subtraction)
+
+print ((4*2)/8 + (18+20))
+
+#To writeformula in multiple line we need back slash \
+result = 12 * 4 + 8 * 2 + \
+          4 * 2 + 6 * 8 + 10
+print (result)
+
+# end delimiter to remove new line , By default print will switch to another Statement
+First_name = "Fathima haifa"
+print (name , )

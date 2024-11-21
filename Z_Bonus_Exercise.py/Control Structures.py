eaf3953f5ef3630f4da5225edef7551d132e20d5
Haifa2006 <fathimahaifahaneef.2006@gmail.com>
2024-11-21 21:35:52 +0400

    Control Structures.py

---
+++ Z_Bonus_Exercise.py/Control Structures.py
@@ -0,0 +1,23 @@
+#Control Structures:
+"""Imagine an alien was just shot down in a game. Create a variable called alien_color and
+assign it a value of 'green', 'yellow', or 'red'.
+•Write an if statement to test whether the alien’s color is green. If it is, print a message
+that the player just earned 5 points.
+•Write one version of this program that passes the if test and another that fails. (The
+version that fails will have no output.)"""
+
+#Answers:
+#Setting the color for the alien
+alien_color = "Green"
+
+#Verify if the alien's color is green
+if alien_color == "Green":
+    print("Well done! You've scored 5 points.")
+
+
+#Setting the color for the alien
+alien_color = "Red"
+
+#Verify if the alien's color is green
+if alien_color == "green":
+    print("Well done! You've scored 5 points.")

1cdcfb1d4cb5db7ec620ce01a40e4b25490275b7
Haifa2006 <fathimahaifahaneef.2006@gmail.com>
2024-11-21 20:49:31 +0400

    String Concatenation.py

---
+++ Y_Class_Activities.py/String Concatenation.py
@@ -0,0 +1,46 @@
+#String Concatenation
+
+x= "Bathspa"
+print(type(x))
+
+y= 10
+print(type(y))
+
+Name = "University"
+print(Name)
+
+#1 String concatenation through variable 
+message = "Hello " + "World"
+print(message)
+
+#2
+hello = "Welcome to " + "the world"
+print(hello)
+
+#3
+name = "Fathima " + "Haifa"
+print(name)
+
+#1 String concatenation through print statement 
+print("Hello " + "World ")
+
+#2
+print("Welcome to " +
+      "Bathspa university" )
+
+#3
+print("I have " +
+      "2 Siblings")
+
+#1 String concatenation through Diff Variables
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+#2
+Name = "Fathima Haifa "
+Age = "18 Years "
+Place = "Ajman "
+Result = Name + " " + Age + " " + Place
+print(Result)
